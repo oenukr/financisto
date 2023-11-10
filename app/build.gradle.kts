e07792188a832d61aa9724c5915bc05a4001bc33
oenukr <oen.ukr@gmail.com>
2023-11-10 13:43:35 +0100

    robolectric 4.7.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -130,7 +130,7 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     testImplementation("androidx.test:core:1.5.0")
 //    testImplementation("org.robolectric:robolectric:4.11.1") // can't be used because of the current target sdk
-    testImplementation("org.robolectric:robolectric:4.3.1")
+    testImplementation("org.robolectric:robolectric:4.7.3")
     testCompileOnly("org.hamcrest:hamcrest:2.2")
 }
 
