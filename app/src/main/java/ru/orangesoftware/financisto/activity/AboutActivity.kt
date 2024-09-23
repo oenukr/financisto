4158fce2a807205894061e4d7d2fde35d1737988
oenukr <oen.ukr@gmail.com>
2024-09-23 10:10:33 +0200

    refactor: rewrite AboutActivity in Jetpack Compose
    Rewrote AboutActivity using Jetpack Compose and accompanist webview.
    Updated AGP and accompanist libraries to the latest versions.

--- app/src/main/java/ru/orangesoftware/financisto/activity/AboutActivity.kt
+++ app/src/main/java/ru/orangesoftware/financisto/activity/AboutActivity.kt
@@ -1,75 +1,139 @@
-/*
- * Copyright (c) 2011 Denis Solonenko.
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the GNU Public License v2.0
- * which accompanies this distribution, and is available at
- * https://www.gnu.org/licenses/old-licenses/gpl-2.0.html
- */
+package ru.orangesoftware.financisto.activity
 
-package ru.orangesoftware.financisto.activity;
+import android.content.Context
+import android.content.pm.PackageManager
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Tab
+import androidx.compose.material3.TabRow
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import com.google.accompanist.web.WebView
+import com.google.accompanist.web.rememberWebViewState
+import ru.orangesoftware.financisto.R
+import ru.orangesoftware.financisto.utils.MyPreferences
+import ru.orangesoftware.financisto.utils.Utils
 
-import android.app.TabActivity;
-import android.content.Context;
-import android.content.Intent;
-import android.content.pm.PackageInfo;
-import android.content.pm.PackageManager;
-import android.os.Bundle;
-import android.widget.TabHost;
-
-import androidx.core.content.ContextCompat;
-
-import ru.orangesoftware.financisto.R;
-import ru.orangesoftware.financisto.utils.MyPreferences;
-import ru.orangesoftware.financisto.utils.Utils;
-
-/**
- * Created by IntelliJ IDEA.
- * User: Denis Solonenko
- * Date: 3/24/11 10:20 PM
- */
-public class AboutActivity extends TabActivity {
-
-    @Override
-    protected void attachBaseContext(Context base) {
-        super.attachBaseContext(MyPreferences.switchLocale(base));
+class AboutActivity : ComponentActivity() {
+    override fun attachBaseContext(base: Context) {
+        super.attachBaseContext(MyPreferences.switchLocale(base))
     }
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState ) {
-        super.onCreate(savedInstanceState);
-        setTitle("Financisto ("+getAppVersion(this)+")");
-
-        addTabForFile("whatsnew", R.string.whats_new);
-        addTabForUrl("https://financisto.com/privacy.html", R.string.privacy_policy);
-        addTabForFile("gpl-2.0-standalone", R.string.license);
-        addTabForFile("about", R.string.about);
-    }
-
-    private void addTabForFile(String name, int titleId) {
-        Intent intent = new Intent(this, WebViewActivity.class);
-        intent.putExtra(WebViewActivity.FILENAME, name);
-        TabHost tabHost = getTabHost();
-        tabHost.addTab(tabHost.newTabSpec(name)
-                .setIndicator(getString(titleId), ContextCompat.getDrawable(this, R.drawable.ic_tab_about))
-                .setContent(intent));
-    }
-
-    private void addTabForUrl(String url, int titleId) {
-        Intent intent = new Intent(this, WebViewActivity.class);
-        intent.putExtra(WebViewActivity.URL, url);
-        TabHost tabHost = getTabHost();
-        tabHost.addTab(tabHost.newTabSpec(String.valueOf(titleId))
-                .setIndicator(getString(titleId), ContextCompat.getDrawable(this, R.drawable.ic_tab_about))
-                .setContent(intent));
-    }
-
-    public static String getAppVersion(Context context) {
-        try {
-            PackageInfo info = Utils.getPackageInfo(context);
-            return "v. "+info.versionName;
-        } catch (PackageManager.NameNotFoundException e) {
-            return "";
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            FinancistoTheme {
+                AboutScreen()
+            }
         }
     }
 
+    @Composable
+    fun FinancistoTheme(content: @Composable () -> Unit) {
+        MaterialTheme(
+            content = content
+        )
+    }
+
+    @OptIn(ExperimentalMaterial3Api::class)
+    @Composable
+    fun AboutScreen() {
+        var state by remember { mutableIntStateOf(0) }
+        val titles = listOf(
+            R.string.whats_new,
+            R.string.privacy_policy,
+            R.string.license,
+            R.string.about,
+        )
+
+        Scaffold(
+            topBar = {
+                TopAppBar(title = { Text("Financisto (${getAppVersion(this)})") })
+            }
+        ) { paddingValues ->
+            Column(modifier = Modifier.padding(paddingValues)) {
+                TabRow(selectedTabIndex = state) {
+                    titles.forEachIndexed { index, title ->
+                        Tab(
+                            text = { Text(stringResource(id = title)) },
+                            selected = state == index,
+                            onClick = { state = index },
+                            icon = {
+                                Icon(
+                                    painter = painterResource(
+                                        if (state == index) {
+                                            R.drawable.ic_tab_android_active
+                                        } else {
+                                            R.drawable.ic_tab_android_inactive
+                                        }
+                                    ),
+                                    contentDescription = null
+                                )
+                            },
+                        )
+                    }
+                }
+                when(state) {
+                    0 -> WhatsNewContent()
+                    1 -> PrivacyPolicyContent()
+                    2 -> LicenseContent()
+                    3 -> AboutContent()
+                }
+            }
+        }
+    }
+
+    @Composable
+    fun WhatsNewContent() {
+        val url = "file:///android_asset/whatsnew.htm"
+        val state = rememberWebViewState(url = url)
+        WebView(state)
+    }
+
+    @Composable
+    fun PrivacyPolicyContent() {
+        val url = "https://financisto.com/privacy.html"
+        val state = rememberWebViewState(url = url)
+        WebView(state)
+    }
+
+    @Composable
+    fun LicenseContent() {
+        val url = "file:///android_asset/gpl-2.0-standalone.htm"
+        val state = rememberWebViewState(url = url)
+        WebView(state)
+    }
+
+    @Composable
+    fun AboutContent() {
+        val url = "file:///android_asset/about.htm"
+        val state = rememberWebViewState(url = url)
+        WebView(state)
+    }
+
+    companion object {
+        fun getAppVersion(context: Context): String {
+            return try {
+                val info = Utils.getPackageInfo(context)
+                "v. " + info.versionName
+            } catch (e: PackageManager.NameNotFoundException) {
+                ""
+            }
+        }
+    }
 }
