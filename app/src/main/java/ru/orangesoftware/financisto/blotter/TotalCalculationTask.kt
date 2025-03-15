2475b4103224c4ec4de5f79b04442f7c36c4ef96
oenukr <oen.ukr@gmail.com>
2025-03-15 13:37:56 +0100

    changed the indentation

--- app/src/main/java/ru/orangesoftware/financisto/blotter/TotalCalculationTask.kt
+++ app/src/main/java/ru/orangesoftware/financisto/blotter/TotalCalculationTask.kt
@@ -12,38 +12,39 @@ import ru.orangesoftware.financisto.model.Total
 import ru.orangesoftware.financisto.utils.Utils
 
 abstract class TotalCalculationTask(
-	private val context: Context,
-	private val totalText: TextView?,
+    private val context: Context,
+    private val totalText: TextView?,
 ) : AsyncTask<Any, Total, Total>() {
 
-	@Volatile
-	private var isRunning: Boolean = true
+    @Volatile
+    private var isRunning: Boolean = true
 
-	@Deprecated("Deprecated in Java")
-	override fun doInBackground(vararg params: Any?): Total {
-		try {
-		    return getTotalInHomeCurrency()
-		} catch (ex: Exception) {
-			Log.e("TotalBalance", "Unexpected error", ex)
-			return Total.ZERO
-		}
-	}
+    @Deprecated("Deprecated in Java")
+    override fun doInBackground(vararg params: Any?): Total {
+        try {
+            return getTotalInHomeCurrency()
+        } catch (ex: Exception) {
+            Log.e("TotalBalance", "Unexpected error", ex)
+            return Total.ZERO
+        }
+    }
 
-	abstract fun getTotalInHomeCurrency(): Total
-	abstract fun getTotals(): Array<Total>
+    abstract fun getTotalInHomeCurrency(): Total
+    abstract fun getTotals(): Array<Total>
 
-	@Deprecated("Deprecated in Java")
-	override fun onPostExecute(result: Total?) {
-		if (isRunning) {
-			if (result?.currency == Currency.EMPTY) {
-				Toast.makeText(context, R.string.currency_make_default_warning, Toast.LENGTH_LONG).show()
-			}
-			val u = Utils(context)
-			u.setTotal(totalText, result)
-		}
-	}
+    @Deprecated("Deprecated in Java")
+    override fun onPostExecute(result: Total?) {
+        if (isRunning) {
+            if (result?.currency == Currency.EMPTY) {
+                Toast.makeText(context, R.string.currency_make_default_warning, Toast.LENGTH_LONG)
+                    .show()
+            }
+            val u = Utils(context)
+            u.setTotal(totalText, result)
+        }
+    }
 
-	fun stop() {
-		isRunning = false
-	}
+    fun stop() {
+        isRunning = false
+    }
 }
